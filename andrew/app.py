f084051b5ddaf65bb63c9ccaaed071255e618d21
RyanMacc <arbader2142@gmail.com>
2023-05-04 16:17:45 -0400

    update streamlit interface with 3 columns and 4 inputs

--- andrew/app.py
+++ andrew/app.py
@@ -1,14 +1,23 @@
 import streamlit as st
 import pandas as pd
 
-st.write("# Python Web App")
+# Create the layout of the app with three columns
+col1, col2, col3 = st.columns(3)
 
-st.write("Hi, this is our first web app in Python! :sunglasses:")
+# Add content to the first column
+with col1:
+    st.write("This is the first column")
 
-df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
-st.write(df)
+# Add content to the third column
+with col3:
+    st.write("This is the third column")
 
-input_value = st.text_input("Enter a Message")
-
-if st.button("Display Message"):
-    st.write(input_value)
+# Add content to the second column
+with col2:
+    st.write("This is the second column")
+    
+    # Add four input widgets to the second column
+    input1 = st.text_input("Enter value 1")
+    input2 = st.text_input("Enter value 2")
+    input3 = st.text_input("Enter value 3")
+    input4 = st.text_input("Enter value 4")
