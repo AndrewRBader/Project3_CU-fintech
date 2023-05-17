334947ed96dc9ac89b4754597039380bd869bf1d
trentransom <116039236+trentransom@users.noreply.github.com>
2023-05-17 14:29:15 -0400

    commetns

--- final_app/app.py
+++ final_app/app.py
@@ -39,8 +39,9 @@ cash_list = st.session_state.payment_list
 st.write(cash_list)
 
 ################################################################################
-# Register New Asset, require name, type, price, details
+# Register New Asset, require name, type, price, details (+ document --> optional)
 ################################################################################
+
 #make a load_contract that will bring in the json abi file
 @st.cache(allow_output_mutation=True)
 def load_contract():
@@ -55,13 +56,15 @@ def load_contract():
     )
 
     return contract
+
 # connect the contract variable to the load_contract function
 contract = load_contract()
 
-
+# Initialize asset_list in st.session_state
 if 'asset_list' not in st.session_state:
     st.session_state.asset_list = []
 
+# Gather information from user
 st.markdown("## Register New Asset")
 nft_name = st.text_input("Enter the name of the asset")
 nft_type = st.text_input("Enter the asset type")
@@ -70,6 +73,7 @@ details = st.text_input("Enter any details you have")
 nft_uri = "N/A"
 nft_recipient_address = st.text_input("Enter the Ethereum address of the inheritor")
 if st.button("Register Asset"):
+    # Execute the transaction minting an NFT to the User's account.
     tx_hash = contract.functions.tokenizeAsset(
         nft_name,
         nft_type, 
@@ -78,18 +82,27 @@ if st.button("Register Asset"):
         nft_uri
     ).transact({'from': address})
     receipt=w3.eth.waitForTransactionReceipt(tx_hash)
+
+    # Create an event to access the token ID of the newly minted nft
     events_filter = contract.events.NFTTokenId.createFilter(
         fromBlock='latest'
     )
     events = events_filter.get_all_entries()
     for event in events:
         event_dictionary = dict(event)
+        # Access the token ID
         nft_transfer = {'nft_recipient_address': nft_recipient_address, 'token_id': event_dictionary['args']['tokenId']}
+        # Add the token ID mapped to the account it will be
+        # sent to upon execution of the will.
         st.session_state.asset_list.append(nft_transfer)
+        # Print out the token id of the newly minted nft along
+        # along with the account it will be sent to.
         st.write('Event Log')
         st.write(event_dictionary)
         st.write('Token ID')
         st.write(st.session_state.asset_list)
+# Save the list of token IDs mapped to accounts
+# for use later upon execution of the will.
 asset_list = st.session_state.asset_list
 st.markdown("---")
 
@@ -134,19 +147,31 @@ if st.button("Execute"):
         # Convert balance from ether to wei
         value = w3.toWei(amount, 'ether')
         # Send the transaction to the blockchain
-        w3.eth.send_transaction({'to': receiver , 'from': sender , 'gas': gas, 'value': value})
+        tx_hash = w3.eth.send_transaction({
+            'to': receiver , 
+            'from': sender , 
+            'gas': gas, 
+            'value': value
+            })
+        receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+        st.write(dict(receipt))
+        st.write(f'You sent {amount} to {receiver}')
 
     # Transfer the assets
     for x in asset_list:
+        # Save the receiver and the token ID 
+        # of each NFT to perform the transaction.
         receiver = x['nft_recipient_address']
         token_id = x['token_id']
+        # Send the token to the account the user specified 
+        # when creating the will.
         tx_hash = contract.functions.transferNFT(
             address, 
             receiver, 
             token_id
             ).transact({'from': address})
         receipt=w3.eth.waitForTransactionReceipt(tx_hash)
-        st.write(receipt)
+        st.write(dict(receipt))
         st.markdown("### Asset Owner")
         st.write(f'Token ID: {token_id}')
         st.write(contract.functions.getOwner(token_id).call())
