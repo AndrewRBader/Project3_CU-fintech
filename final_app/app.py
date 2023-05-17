5d9df0ea118fc1892bc97e7c7cbd8674aa3d6ce2
halamkim <116533079+halamkim@users.noreply.github.com>
2023-05-17 15:08:45 -0400

    Organizing the order, adding comments to my code, changing the front end of the execution print function

--- final_app/app.py
+++ final_app/app.py
@@ -13,35 +13,6 @@ load_dotenv()
 w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 
 
-
-######################################################################################
-st.title("Asset Registry")
-accounts = w3.eth.accounts
-address = w3.eth.accounts[0] 
-st.markdown("---")
-
-################################################################################
-# Register Cash
-################################################################################
-st.markdown("## Register Cash Distribution")
-
-if 'payment_list' not in st.session_state:
-    st.session_state.payment_list = []
-
-recipient_address = st.text_input("Enter recipient address:")
-amount = st.number_input("Enter amount:")
-
-if st.button("Register"):
-    payment = {'recipient_address': recipient_address, 'amount': amount}
-    st.session_state.payment_list.append(payment)
-
-cash_list = st.session_state.payment_list
-st.write(cash_list)
-
-################################################################################
-# Register New Asset, require name, type, price, details (+ document --> optional)
-################################################################################
-
 #make a load_contract that will bring in the json abi file
 @st.cache(allow_output_mutation=True)
 def load_contract():
@@ -60,6 +31,51 @@ def load_contract():
 # connect the contract variable to the load_contract function
 contract = load_contract()
 
+
+
+
+################################################################################
+############################ STREAMLIT #########################################
+################################################################################
+
+st.title("Asset Registry")
+# Retrieve the list of accounts from the Ethereum network
+accounts = w3.eth.accounts
+# Select the first account 
+address = accounts[0] 
+st.markdown("---")
+
+################################################################################
+# Register Cash
+################################################################################
+
+# Display a markdown header
+st.markdown("## Register Cash Distribution")
+
+# Check if 'payment_list' key exists in session state, if not, initialize it as an empty list
+if 'payment_list' not in st.session_state:
+    st.session_state.payment_list = []
+
+# Gather user input
+recipient_address = st.text_input("Enter recipient address:")
+amount = st.number_input("Enter amount:")
+
+if st.button("Register"):
+    # Create a dictionary with the recipient address and amount
+    payment = {'recipient_address': recipient_address, 'amount': amount}
+    # Append the payment dictionary to the 'payment_list' in session state
+    st.session_state.payment_list.append(payment)
+
+# Retrieve the 'payment_list' from session state
+cash_list = st.session_state.payment_list
+
+# Display the 'cash_list'
+st.write(cash_list)
+
+################################################################################
+# Register New Asset, require name, type, price, details 
+################################################################################
+
 # Initialize asset_list in st.session_state
 if 'asset_list' not in st.session_state:
     st.session_state.asset_list = []
@@ -72,6 +88,7 @@ price = st.text_input("Enter the value/price of your asset")
 details = st.text_input("Enter any details you have")
 nft_uri = "N/A"
 nft_recipient_address = st.text_input("Enter the Ethereum address of the inheritor")
+
 if st.button("Register Asset"):
     # Execute the transaction minting an NFT to the User's account.
     tx_hash = contract.functions.tokenizeAsset(
@@ -106,29 +123,6 @@ if st.button("Register Asset"):
 asset_list = st.session_state.asset_list
 st.markdown("---")
 
-################################################################################
-# Declare Estate Administrator 
-################################################################################
-st.markdown("## Declare Estate Administrator")
-
-# Create a select box containing a list of available accounts for the user to choose from
-estate_admin = st.selectbox("Select Estate Administrator", options=accounts)
-# "Declare Estate Administrator" button to call the smart contract function
-if st.button("Declare Estate Administrator"):
-    # Call the `declareEstateAdministrator` function with the selected estate administrator address as the argument
-    tx_hash = contract_estate_admin.functions.declareEstateAdministrator(estate_admin).transact({
-        'from': address, 
-        'gas': 1000000})
-    # Wait for the transaction to be mined
-    receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-    st.write("Transaction receipt mined:")
-    st.write(dict(receipt))
-    # catch if fails
-    if contract_estate_admin.functions.estateAdministrators(estate_admin).call() == True:
-        st.write(f"{estate_admin} has been declared as an estate administrator")
-    else:
-        st.write(f"Failed to declare {estate_admin} as an estate administrator")
-
 
 ################################################################################
 # Execution
@@ -153,9 +147,11 @@ if st.button("Execute"):
             'gas': gas, 
             'value': value
             })
+        # Print the cash transaction receipt
+        st.markdown("## Cash Transaction Receipt")
         receipt = w3.eth.waitForTransactionReceipt(tx_hash)
         st.write(dict(receipt))
-        st.write(f'You sent {amount} to {receiver}')
+        st.write(f'The amount of {amount} has been sent to Account: {receiver}')
 
     # Transfer the assets
     for x in asset_list:
@@ -170,9 +166,10 @@ if st.button("Execute"):
             receiver, 
             token_id
             ).transact({'from': address})
+        # Print the NFT registration receipt
+        st.markdown("## Asset Transaction Receipt")
         receipt=w3.eth.waitForTransactionReceipt(tx_hash)
         st.write(dict(receipt))
-        st.markdown("### Asset Owner")
-        st.write(f'Token ID: {token_id}')
-        st.write(contract.functions.getOwner(token_id).call())
+        st.markdown("### Asset Owner:")
+        st.write(f'Asset with the Token ID: {token_id} has been transferred to {contract.functions.getOwner(token_id).call()}')
 
